6cffeb816c01742d205862cb267e86946d580ba8
Réal Provencher-Deshler <real.provencher.deshler@gmail.com>
2024-10-12 11:00:38 -0400

    removed commented out code

--- main.lua
+++ main.lua
@@ -3,10 +3,6 @@ local Systems = {}
 local Assemblages = require("src/assemblages")
 
 local world
-local testEntity
-
---local colorDuration = 0
-
 
 function love.load()
   love.window.setTitle("Hello World")
@@ -23,11 +19,6 @@ function love.load()
   Systems.setBackgroundColorSystem,
   Systems.behaviorSystem
   )
-
-  --Concord.entity(world)
-  --:give("position", 300, 200)
-  --:give("speed", 100)
-  --:give("drawableText", "Hello World")
 
   local backgroundFlasherEntity = Concord.entity(world)
   :give("behavior", {
@@ -64,18 +55,8 @@ end
 
 function love.update(dt)
   world:emit("update", dt)
-
-  --if colorDuration > 0 then
-    --colorDuration = colorDuration - dt
-  --end
 end
 
 function love.draw()
   world:emit("draw")
-
-  --if colorDuration > 0 then
-    --love.graphics.setBackgroundColor(1, 0, 0)
-  --else
-    --love.graphics.setBackgroundColor(0, 0, 0)
-  --end
 end
